05270189f96027005efb18bcf3036ead926542c5
zwj <1790373371@qq.com>
2020-06-06 22:27:28 +0800

    optimization

--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,22 @@ const webpack = require('webpack')
 process.env.NODE_ENV = 'development'
 
 module.exports = {
+    // entry入口点
+    //entry:'./src/index.js' 打包形成一个chunk，输出一个bundle文件，单入口
+    //entry: {index: './src/index.js', index2: './src/index2.js'},多入口,
+    //entry: ['./src/index.js', './src/index2.js'] 只会形成一个chunk输出一个文件，多入口{}和[]可以同时使用
     entry: ['./src/index.js', './src/index.html'],
     output: {
+        // 文件名
         filename: "js/index.[contenthash:10].js",
+        // 输出文件目录
         path: resolve(__dirname, 'dll'),
-        library: '[name]_[hash]'
+        // 所有公共资源引入公共路径前缀
+        publicPath: "/",
+        chunkFilename: "js/[name]_chunk.js", // 非入口chunk的名称
+        // libraryTarget: "window", // 变量名添加到window上
+        library: '[name]_[hash]', // 整个库向外暴露的变量名
+        libraryTarget: "global" // 变量名添加到node上
     },
     module: {
         rules: [
@@ -67,7 +78,7 @@ module.exports = {
                 ]
             },
             {
-                // 其它资源
+                // 排除其它资源
                 exclude: /.(html|js|css|less|jpg|png|jpg|gif)/,
                 loader: 'file-loader',
                 options: {
@@ -79,6 +90,7 @@ module.exports = {
                 loader: 'eslint-loader',
                 enforce: 'pre',
                 exclude: /node_modules/,
+                include: reslove(__dirname, 'src'),
                 options: {
                     fix: true // 自动修复
                 }
@@ -153,18 +165,52 @@ module.exports = {
             chunks: 'all'
         }
     },
-    mode: 'development',
+    // mode: 'development',
+    mode: 'production',
     // 只在内存中打包
     // 启动devServer: webpack-dev-server(npm i webpack-dev-server -D)，运行：npx webpack-dev-server
     devServer: {
+        // 运行代码的目录
         contentBase: resolve(__dirname, 'build'),
+        // 监视contentBase目录下的所有文件，一旦文件变化就reload
+        watchContentBase: true,
+        // 开启压缩
         compress: true,
         port: 3000,
+        // 自动打开浏览器
         open: true,
-        hot: true
+        // 开启HMR
+        hot: true,
+        // 不要显示启动服务日志
+        clientLogLevel: 'none',
+        // 除了一些基本启动信息以外，其它内容不显示
+        quiet: true,
+        // 出错，不要全屏提示
+        overlay: false,
+        // 服务器代理
+        proxy: {
+            '/api': {
+                target: 'http://192.168.11.216',
+                // 路径重写，将/api/xxx --> /xxx(去掉/api)
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
     },
     devtool: "source-map",
     externals: {
         jquery: 'jQuery'
+    },
+    // 解析模块的规则
+    resolve: {
+        // 解析模块路径别名, 优点简写路径，确定没有代码提示
+        alias: {
+            $css: resolve(__dirname, 'src/css') // import '$css/index.css'
+        },
+        // 配置省略文件路径的后缀，帮助省略后缀名
+        extensions: ['.js', '.jsx', '.css'], // import '$css/index'相当于import '$css/index.css'
+        // 告诉webpack解析模块去哪里找, 默认会一层一层往外找
+        modules: [resolve(__dirname, '../../node_modules'), 'node_modules']
     }
 }
